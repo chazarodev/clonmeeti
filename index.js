92dd1bc7f4e1d3aeb9991a8d1cae5e2738638380
Alberto Cházaro <shazarerom@gmail.com>
2021-10-10 16:23:11 -0500

    Primeros pasos, iniciando router

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = require('./routes');
+require('dotenv').config({path: 'variables.env'});
+
+
+const app = express();
+
+// Routing
+app.use('/', router());
+
+// Agrega el puerto
+app.listen(process.env.PORT, () => {
+    console.log('Servidor funcionando');
+})
+
