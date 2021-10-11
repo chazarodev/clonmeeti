d94dea83a8cd5d038a644894f21d797fe2536519
Alberto Cházaro <shazarerom@gmail.com>
2021-10-10 20:12:54 -0500

    Configurando código para mostrar alertas

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,5 +1,22 @@
+const Usuarios = require('../models/Usuarios');
+
 exports.formCrearCuenta = (req, res) => {
     res.render('crear-cuenta', {
         nombrePagina: 'Crea Tu Cuenta'
     })
+}
+
+exports.crearNuevaCuenta = async (req, res) => {
+    const Usuario = req.body
+
+    try {
+        const usuario = await Usuarios.create(Usuario);
+        // TODO flash message y redireccionar
+        console.log(usuario);
+    } catch (error) {
+        const erroresSequelize = error.errors.map(err => err.message);
+        // console.log(erroresSequelize);
+        req.flash('error', erroresSequelize);
+        res.redirect('/crear-cuenta');
+    }
 }
--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const expressEjsLayouts = require('express-ejs-layouts');
 const path = require('path');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const router = require('./routes');
 
-// Importar configuración db
+// Importar configuración db y modelos
 const db = require('./config/db');
 require('./models/Usuarios');
 db.sync().then(() => console.log('db Conectada')).catch((error) => console.log(error));
 
+//Variables de desarrollo
 require('dotenv').config({path: 'variables.env'});
 
-
+//Aplicación principal
 const app = express();
+
+// Body parser, leer formularios
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Habilitar EJS como template engine
 app.use(expressEjsLayouts)
@@ -23,8 +32,23 @@ app.set('views', path.join(__dirname, './views'));
 //Archivos estáticos
 app.use(express.static('public'));
 
+// Habilitar cookie parser
+app.use(cookieParser());
+
+// Crear la sesión
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Agregar flash messages
+app.use(flash());
+
 // Middleware (usuario logueado, flash messages, fecha actual);
 app.use((req, res, next) => {
+    res.locals.mensajes = req.flash();
     const fecha = new Date();
     res.locals.year = fecha.getFullYear();
     next();
