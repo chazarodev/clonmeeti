8053cbd69553e56774d7618d7c094feba4a822f1
Alberto Cházaro <shazarerom@gmail.com>
2021-10-17 15:50:32 -0500

    Filtrando meetis por próximos y anteriores

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,12 +1,22 @@
 const Grupos = require('../models/Grupos');
 const Meeti = require('../models/Meeti');
 const moment = require('moment');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 exports.panelAdministracion = async (req, res) => {
+
     //Consultas
     const consultas = [];
     consultas.push(Grupos.findAll({where: {usuarioId: req.user.id}}));
-    consultas.push(Meeti.findAll({where: {usuarioId: req.user.id}}));
+    consultas.push(Meeti.findAll({
+        where: {
+            usuarioId: req.user.id,
+            fecha: {
+                [Op.gte] : moment(new Date()).format("YYYY-MM-DD")
+            }
+        }
+    }));
 
     //Array destructuring
     const [grupos, meeti] = await Promise.all(consultas);
