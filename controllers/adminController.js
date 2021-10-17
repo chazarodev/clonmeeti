132a9268213dff6363a8734b5699c56a7c601d23
Alberto Cházaro <shazarerom@gmail.com>
2021-10-17 15:28:21 -0500

    Consultando los meetis y pasarlos a la vista

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,11 +1,18 @@
 const Grupos = require('../models/Grupos');
+const Meeti = require('../models/Meeti');
 
 exports.panelAdministracion = async (req, res) => {
-    const grupos = await Grupos.findAll({where: {usuarioId: req.user.id}});
+    //Consultas
+    const consultas = [];
+    consultas.push(Grupos.findAll({where: {usuarioId: req.user.id}}));
+    consultas.push(Meeti.findAll({where: {usuarioId: req.user.id}}));
 
+    //Array destructuring
+    const [grupos, meeti] = await Promise.all(consultas);
 
     res.render('administracion', {
         nombrePagina: 'Panel de Administración',
-        grupos
+        grupos,
+        meeti
     })
 }
